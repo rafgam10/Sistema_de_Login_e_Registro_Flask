d4fc8b7f47ccfff8942b07518bbf5fb424dde2e6
Rafael <rafgam19@gmail.com>
2025-01-15 16:55:57 -0300

    Configuração na função login e base.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -7,13 +7,34 @@ app = Flask(__name__)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        nome = request.form['username']
-        senha = request.form['password']
-        lembreAtivo = bool(request.form['ativo'])
-        if lembreAtivo:
-            print(f"{nome} e {senha}: {lembreAtivo}")
-        return "Login com Sucesso!"
-    return render_template('login.html')
+            
+        nome = request.form.get('username')
+        senha = int(request.form.get('password'))
+        AtivoOrNao = bool(request.form.get('ativo') == "on")
+
+        if nome == "Rafael" and senha == 123 or AtivoOrNao == True:
+            print(f"{nome} e {senha}: {AtivoOrNao}")
+            
+            return redirect(url_for('base'))
+        
+        return render_template('login.html')
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    pass
+
+
+#Base de registro:
+
+registo = []
+
+
+@app.route("/base")
+def base():
+
+
+
+    return render_template("base.html")
 
 
 
